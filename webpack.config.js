fcbb1fe5f1fd532c395544a3f9367508413cdb46
Zeeshan Haider <zhadier39@gmail.com>
2022-01-11 21:14:00 +0500

    add linters to template aswell

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
